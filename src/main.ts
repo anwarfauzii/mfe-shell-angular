ab203258773e120126f5d084d891ecce0adcc46b
anwar.fauzi <anwarfauzi2608@gmail.com>
2023-08-14 17:03:43 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-import('./bootstrap')
-	.catch(err => console.error(err));
+// main.ts
+import { loadRemoteEntry } from '@angular-architects/module-federation';
+
+Promise.all([
+	loadRemoteEntry({ type: 'script', remoteEntry: `http://localhost:4205/remoteEntry.js`, remoteName: 'react'}),
+])
+.catch(err => console.error('Error loading remote entries', err))
+.then(() => import('./bootstrap'))
+.catch(err => console.error(err));
